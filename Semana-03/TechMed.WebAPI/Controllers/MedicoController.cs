009b9d79ee46a6aed0f3255beb1c32141c6afc49
ricardo <ricardo.dev1991@gmail.com>
2024-01-16 17:34:10 -0300

    Criando o controller Médico e adicionando propriedade Guid iD na entidade Medico.

--- Semana-03/TechMed.WebAPI/Controllers/MedicoController.cs
+++ Semana-03/TechMed.WebAPI/Controllers/MedicoController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace TechMed.WebAPI.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("/api/v.1/")]
 public class MedicoController : ControllerBase {
 
     private static readonly string[] Doctors = new[]
@@ -17,14 +17,68 @@ public class MedicoController : ControllerBase {
         _logger = logger;
      }
 
-      [HttpGet(Name = "GetMedico")]
-    
-     public IEnumerable<Medico> Get(){
-        return Enumerable.Range(0, 3).Select(index => new Medico{
+      [HttpGet("medicos")]
+     public IActionResult Get(){
+        var medicos = Enumerable.Range(0, 3).Select(index => new Medico{
+            Id = Guid.NewGuid(),
             Name = Doctors[index],
             Especialidade = "Clinico Geral"
         })
         .ToArray();
+        return Ok(medicos);
      }
 
+     [HttpDelete("/{id}")]
+     public IActionResult Delete(Guid? id){
+         if(id is null) return BadRequest();
+
+        var medicos = Enumerable.Range(0, 3).Select(index => new Medico{
+            Id = Guid.NewGuid(),
+            Name = Doctors[index],
+            Especialidade = "Clinico Geral"
+        })
+        .ToArray();
+        var deletedDoctor = medicos[0].Id;
+        id = deletedDoctor;
+        var newMedicos = medicos.Where(m => m.Id != id);
+        return Ok(newMedicos);
+     }
+
+     [HttpPut("/{id}")]
+       public IActionResult Put(Guid? id){
+         if(id is null) return BadRequest();
+
+        var medicos = Enumerable.Range(0, 3).Select(index => new Medico{
+            Id = Guid.NewGuid(),
+            Name = Doctors[index],
+            Especialidade = "Clinico Geral"
+        })
+        .ToArray();
+        var updatedDoctor = medicos[0].Id;
+        id = updatedDoctor;
+        foreach(var medico in medicos){
+            if(medico.Id == id){
+                medico.Name = "Novo Nome";
+            }
+        }
+        return Ok(medicos);
+     }
+
+     [HttpPost]
+       public IActionResult Post(){
+        var medicos = Enumerable.Range(0, 3).Select(index => new Medico{
+            Id = Guid.NewGuid(),
+            Name = Doctors[index],
+            Especialidade = "Clinico Geral"
+        })
+        .ToArray();
+        
+       var newMedico = medicos.Append(new Medico{
+            Id = Guid.NewGuid(),
+            Name = "Dr Paulo",
+            Especialidade = "Clinico Geral"
+        });
+
+        return Ok(newMedico);
+     }
 }
--- Semana-03/TechMed.WebAPI/Medico.cs
+++ Semana-03/TechMed.WebAPI/Medico.cs
@@ -2,6 +2,7 @@ namespace TechMed.WebAPI;
 
 public class Medico
 {
+    public Guid Id { get; set; }
     required public string Name { get; set; }
     required public string Especialidade { get; set; }
 }
